2627d9153c7ca8acb58b76484f4f7b066abe3c05
Josiah Rooney <josiahrooney@gmail.com>
2023-02-04 23:31:15 -0800

    Revert "Add redux"
    This reverts commit 2c537877c0012728146037a122e38e22a6b88c06.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,14 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-
-import reportWebVitals from "./reportWebVitals"
-import App from "./App"
-import store from "./ducks/store"
-
 import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )
 
--- src/utils/lib.ts
+++
@@ -1,3 +0,0 @@
-export const renderDescription = (description: string) => {
-  return 1
-}
